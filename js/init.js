707f86dee39b1b60375e1db2023d6e62e1c7bcf3
giorodriguez1984 <giovanni1984@msn.com>
2017-03-17 21:55:10 -0400

    fix assets

---
+++ js/init.js
@@ -0,0 +1,10 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    
+    $('.slider').slider();
+
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
